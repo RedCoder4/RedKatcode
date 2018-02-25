321d92f75fb5dcc3fd410e7b1b5b63f4d3694801
RedCoder4 <bd5@hotmail.com.au>
2018-02-25 13:05:18 +1100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('RedKatCode, connected and working. ');
+
+           client.user.setPresence({ status: 'idle', game: { name: "%SQL STAUS: BAD" } });
+});
+
+
+client.on('message', message => {
+    if (message.content === '?news') {
+    	message.channel.send('Most of the news is submitted to the Welcome channel, go and check it out!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '?love') {
+    	message.reply(':heart: I love you too');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '?help') {
+    	message.channel.send(':book: ?news - Will display the news from a dB table.');
+        message.channel.send(':book: ?love - Showing some love awwwww :heart:');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === '?raw') {
+    	message.channel.send('Version 1.0');
+        message.channel.send(':computer: MySQL status :name_badge: DOWN');
+          	}
+});
+
+
+// Input into config VARS for the token to connect. 
+
+client.login(process.env.BOT_TOKEN);
